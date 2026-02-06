e089edddf7740ae4f83fb2f70a8decb7896df7ac
UdaraDewshan <dewudara@gmail.com>
2026-02-06 23:27:36 +0530

    Add PostCSS and Tailwind CSS dependencies to package.json and create .postcssrc.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [HomeComponent]
 })
-export class AppComponent {
-  title = 'burgerShop';
+export class AppComponent implements OnInit {
+  title = 'burger_Shop';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
