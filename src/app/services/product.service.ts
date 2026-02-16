e3cc5a0400d191e2adf42a9e68463943c858191c
UdaraDewshan <dewudara@gmail.com>
2026-02-16 23:52:23 +0530

    Add HttpClient provider to app configuration and implement ProductService with API URL

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { product } from '../model/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private url : any = 'https://fakestoreapi.com/products';
+
+  constructor(private http : HttpClient) { }
+
+  getProducts(){
+
+  }
+
 }
